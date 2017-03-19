434a8ea55b99b404f9c9d0085d1e3efd29e9f01a
Ben Pomerenke <pomerenb@asynchrony.com>
2017-03-19 13:59:45 -0500

    000 - splitting out html template - BP

--- SpotifyMusicPlayer/src/app/app.component.ts
+++ SpotifyMusicPlayer/src/app/app.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
+  moduleId: module.id,
   selector: 'spotify-music-player',
-  template: `<h1>Hello {{name}}</h1>`
+  templateUrl: 'app.component.html'
 })
 export class AppComponent { name = 'Angular'; }
