13ebbea8192f56f06fe6ee86ea91192ec88000e1
Ben Pomerenke <pomerenb@asynchrony.com>
2017-03-20 23:27:13 -0500

    002 - added wavesurfer markup and style - BP

--- SpotifyMusicPlayer/src/app/playlist/playlist.component.ts
+++ SpotifyMusicPlayer/src/app/playlist/playlist.component.ts
@@ -4,6 +4,8 @@ import { ActivatedRoute, Params, Router } from '@angular/router';
 import { PlaylistService } from "../services/playlist.service";
 import { Track } from "./track";
 
+declare var WaveSurfer: any; 
+
 @Component({
     moduleId: module.id,
     selector: 'playlist-component',
@@ -13,8 +15,18 @@ export class PlaylistComponent implements OnInit {
     constructor(private router: Router, private activatedRoute: ActivatedRoute, private playlistService: PlaylistService) { }
 
     playlist: Track[];
+    waveSurfer: any;
 
     ngOnInit(): void {
+        this.waveSurfer = Object.create(WaveSurfer);
+        this.waveSurfer.init({
+            container: document.querySelector('#wave'),
+            cursorColor: '#aaa',
+            cursorWidth: 1,
+            height: 80,
+            waveColor: '#588efb',
+            progressColor: '#f043a4'
+        });
         this.playlistService.getPlaylist().then((tracks) => {
             this.playlist = tracks;
         });
